e33d16638743c34eab6386d18279cf0135cae15a
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2025-07-08 16:21:40 +0100

    Use Lazyloading Concept

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component.js';
-import { AboutComponent } from './about/about.component.js';
-import { PortfolioComponent } from './portfolio/portfolio.component.js';
-import { ContactComponent } from './contact/contact.component.js';
-import { NotfoundComponent } from './notfound/notfound.component.js';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, title: 'home' },
-    { path: 'about', component: AboutComponent, title: 'about' },
-    { path: 'portfolio', component: PortfolioComponent, title: 'portfolio' },
-    { path: 'contact', component: ContactComponent, title: 'contact' },
-    { path: '**', component: NotfoundComponent, title: '404' } // Wildcard route for a 404 page
+    { path: '', loadComponent: ()=>import('./home/home.component.js').then((m)=>m.HomeComponent), title: 'home' },
+    { path: 'about', loadComponent: ()=>import('./about/about.component.js').then((m)=>m.AboutComponent), title: 'about' },
+    { path: 'portfolio', loadComponent: ()=>import('./contact/contact.component.js').then((m)=>m.ContactComponent), title: 'portfolio' },
+    { path: 'contact', loadComponent: ()=>import('./portfolio/portfolio.component.js').then((m)=>m.PortfolioComponent), title: 'contact' },
+    { path: '**', loadComponent: ()=>import('./notfound/notfound.component.js').then((m)=>m.NotfoundComponent), title: '404' } // Wildcard route for a 404 page
 ];
