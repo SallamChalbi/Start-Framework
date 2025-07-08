730cc722c08bbae271333492864dd9846cb7f54c
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2025-07-08 19:47:18 +0100

    make scroll start from top

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling, InMemoryScrollingOptions, InMemoryScrollingFeature } from '@angular/router';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+const scrollConfig: InMemoryScrollingOptions = {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    };
+const inMemoryScrollingFeature: InMemoryScrollingFeature = withInMemoryScrolling(scrollConfig);
+
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes, withHashLocation(), inMemoryScrollingFeature),
   ]
 }).catch(err => console.error(err));
